23cf4f9bc32171e32b1fe8e35e5b1f5bfc4d5c3d
Brian Kresge <brian.kresge@gmail.com>
2016-08-05 14:56:47 -0400

    1.6 post item change set

---
+++ TechDashboardWEBAPI/ServiceCode/TdWsService_SO_SalesOrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using Rkl.Erp.Sage.Sage100.TableObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechDashboardWEBAPI.ServiceCode
+{
+    public partial class TdWsService
+    {
+        public List<SO_SalesOrderDetail> GetSDataForSO_SalesOrderDetailFiltered(string filterType, string filterText)
+        {
+            return GetData<SO_SalesOrderDetail>(filterType, filterText);
+        }
+
+        public List<SO_SalesOrderDetail> GetSDataForSO_SalesOrderDetailAll()
+        {
+            return GetData<SO_SalesOrderDetail>(string.Empty, string.Empty);
+        }
+    }
+}
